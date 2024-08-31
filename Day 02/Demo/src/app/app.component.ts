f38634aa996fb68c7ad190f1057582ae51c84e90
Yousef Saad <saady8454@gmail.com>
2024-09-01 02:48:48 +0300

    All in one component

--- Day 02/Demo/src/app/app.component.ts
+++ Day 02/Demo/src/app/app.component.ts
@@ -4,6 +4,7 @@ import { FirstComponent } from './Components/First/First.component';
 import { SecondComponent } from './Components/second/second.component';
 import { Task1Component } from './Components/task1/task1.component';
 import { Task2Component } from './Components/task2/task2.component';
+import { AllInOneComponent } from './Components/all-in-one/all-in-one.component';
 
 // each component contains 4 Parts :
 //  1- tmplate
@@ -11,14 +12,21 @@ import { Task2Component } from './Components/task2/task2.component';
 //  3- logic
 //  4- selector
 
-
 @Component({
   selector: 'app-root', // selector
   standalone: true, //
-  imports: [RouterOutlet,FirstComponent,SecondComponent,Task1Component,Task2Component],
+  imports: [
+    RouterOutlet,
+    FirstComponent,
+    SecondComponent,
+    Task1Component,
+    Task2Component,
+    AllInOneComponent,
+  ],
   templateUrl: './app.component.html', // template
-  styleUrl: './app.component.css' // style
+  styleUrl: './app.component.css', // style
 })
-export class AppComponent { // logic
+export class AppComponent {
+  // logic
   title = 'Demo';
 }
